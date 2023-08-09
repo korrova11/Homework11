df9bdd601e9b39e00ba158b349fbe8f6da7b3362
korrova11 <korrova@yandex.ru>
2023-08-09 16:43:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+    }
+}
